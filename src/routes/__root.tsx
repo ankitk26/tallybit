ed413d6037956487017fa0934d1bd75d21292f8a
ankitk26 <myselfankit51@gmail.com>
2025-12-28 19:15:34 +0530

    add tailwind

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,6 +6,7 @@ import {
   HeadContent,
   Scripts,
 } from "@tanstack/react-router";
+import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
   head: () => ({
@@ -21,6 +22,7 @@ export const Route = createRootRoute({
         title: "TanStack Start Starter",
       },
     ],
+    links: [{ rel: "stylesheet", href: appCss }],
   }),
   component: RootComponent,
 });
